bdd833a69082a15249033f08b282af41445af5a9
Keidi Teixeira Barbieri <keidi_barbieri@estudante.sesisenai.org.br>
2023-07-18 08:58:20 -0300

    piso salarial

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcularSalario() {
+    const salarioAnual = parseFloat(document.querySelector("input[name=salario_anual]").value);
+    const pisoSalarial = parseFloat(document.querySelector("input[name=piso_salarial]").value);
+  
+    const salarioMensal = salarioAnual / 12;
+  
+    if (salarioMensal >= pisoSalarial) {
+        document.getElementById("resultado").innerHTML = "O salário mensal é de R$" + salarioMensal.toFixed(2) + " e está de acordo com o piso.";
+    } else {
+        document.getElementById("resultado").innerHTML = "O salário mensal é de R$" + salarioMensal.toFixed(2) + " e está abaixo do piso.";
+    }
+}
